bf44a4fc6bb7a1565fa8b2dfa69f8e61b59c0694
Bahram2006 <yardam034@gmail.com>
2026-04-19 12:13:49 +0500

    added bottom tab icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,23 +1,36 @@
 import { Tabs } from "expo-router";
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function TabLayout() {
   const { cart } = useContext(CartContext);
 
   return (
-    <Tabs>
-      <Tabs.Screen 
-        name="index" 
-        options={{ title: "Home" }} 
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#d62828",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
       />
 
-      <Tabs.Screen 
-        name="cart" 
-        options={{ 
+      <Tabs.Screen
+        name="cart"
+        options={{
           title: "Cart",
-          tabBarBadge: cart.length > 0 ? cart.length : undefined
-        }} 
+          tabBarBadge: cart.length > 0 ? cart.length : undefined,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="cart" size={size} color={color} />
+          ),
+        }}
       />
     </Tabs>
   );
