b01529a4c2318d53803b9d6a8168b57fbae26b00
Bahram2006 <yardam034@gmail.com>
2026-04-19 12:16:04 +0500

    added bottom press animation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,61 @@
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  Animated,
+} from "react-native";
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 
 const burgers = [
-  { id: "1", name: "Whopper", price: 6.99, image: require("../../assets/images/whopper.png") },
-  { id: "2", name: "Cheese Burger", price: 4.99, image: require("../../assets/images/cheese.png") },
-  { id: "3", name: "Chicken Burger", price: 5.49, image: require("../../assets/images/chicken.png") },
-  { id: "4", name: "Double King Burger", price: 8.99, image: require("../../assets/images/double.png") },
+  {
+    id: "1",
+    name: "Whopper",
+    price: 6.99,
+    image: require("../../assets/images/whopper.png"),
+  },
+  {
+    id: "2",
+    name: "Cheese Burger",
+    price: 4.99,
+    image: require("../../assets/images/cheese.png"),
+  },
+  {
+    id: "3",
+    name: "Chicken Burger",
+    price: 5.49,
+    image: require("../../assets/images/chicken.png"),
+  },
+  {
+    id: "4",
+    name: "Double King Burger",
+    price: 8.99,
+    image: require("../../assets/images/double.png"),
+  },
 ];
 
 export default function HomeScreen() {
   const { addToCart } = useContext(CartContext);
+
+  const scale = new Animated.Value(1);
+
+  const animatePress = () => {
+    Animated.sequence([
+      Animated.timing(scale, {
+        toValue: 0.9,
+        duration: 100,
+        useNativeDriver: true,
+      }),
+      Animated.timing(scale, {
+        toValue: 1,
+        duration: 100,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
 
   return (
     <View style={styles.container}>
@@ -23,19 +68,22 @@ export default function HomeScreen() {
         contentContainerStyle={{ paddingTop: 20 }}
         renderItem={({ item }) => (
           <View style={styles.card}>
-            
             <Image source={item.image} style={styles.image} />
 
             <Text style={styles.name}>{item.name}</Text>
             <Text style={styles.price}>${item.price}</Text>
 
-            <TouchableOpacity 
-              style={styles.button} 
-              onPress={() => addToCart(item)}
-            >
-              <Text style={styles.buttonText}>Add to Cart</Text>
-            </TouchableOpacity>
-
+            <Animated.View style={{ transform: [{ scale }] }}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                  animatePress();
+                  addToCart(item);
+                }}
+              >
+                <Text style={styles.buttonText}>Add to Cart</Text>
+              </TouchableOpacity>
+            </Animated.View>
           </View>
         )}
       />
