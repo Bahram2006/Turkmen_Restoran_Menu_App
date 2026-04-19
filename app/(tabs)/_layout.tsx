d38597dfb45529c314ba14f0ae1d432288c3d2b3
Bahram2006 <yardam034@gmail.com>
2026-04-19 11:33:35 +0500

    added cart badge count in tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,23 @@
 import { Tabs } from "expo-router";
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
 
 export default function TabLayout() {
+  const { cart } = useContext(CartContext);
+
   return (
     <Tabs>
       <Tabs.Screen 
         name="index" 
         options={{ title: "Home" }} 
       />
-      
+
       <Tabs.Screen 
         name="cart" 
-        options={{ title: "Cart" }} 
+        options={{ 
+          title: "Cart",
+          tabBarBadge: cart.length > 0 ? cart.length : undefined
+        }} 
       />
     </Tabs>
   );
